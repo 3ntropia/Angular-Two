2adbf0fac7b1afe11c9c7db3384cd9f98c3aab63
mhuber <mhuber@hexacta.com>
2017-09-07 15:32:22 -0300

    Tour completed.

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+import { HeroService } from './hero.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+
+}
+
+
